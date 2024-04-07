72206066ee1cb0f4b407d17bdf5c9985c32b0924
SuperTurtleIdle <140896354+SuperTurtleIdle@users.noreply.github.com>
2024-04-07 14:06:58 +0200

    Update patreon.js

--- patreon.js
+++ patreon.js
@@ -1,5 +1,5 @@
 let patreonTier1 = ["tortulily", "invisibilia_draconis"]
-let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217"]
+let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "usajoyrider"]
 let patreonTier3 = ["froza", "RC_Juicebox"]
 
 let patreonTier1Alt = ["Dragomura"]
