56a17b9d4fa0b4e8bb02b183aa5cc94fd2a66c89
SuperTurtleIdle <drvcxrf2@gmail.com>
2024-07-18 20:26:51 +0200

    0.4 initial
    ay

--- patreon.js
+++ patreon.js
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-let patreonTier1 = ["tortulily", "huskymudkipper56", "nekosity", "kidsmeal", "pursuantaunt", "Jacozy", "Kasey"]
-let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "usajoyrider", "artgor", "mandlz", "maplecream.", "kgmg", "heinrich1960", "akerson", "johnnydela.", "occultjay", "rugiesm", "kevink6665"]
-let patreonTier3 = ["froza", "RC_Juicebox", "graham anderson", "E", "Steven Burton", "Sarah", "SaWright", "Okko Suga", "Nominae", "Ayse Gursoy", "Mike Muzurakis"]
-
-let patreonTier1Alt = ["Mudkip"]
-let patreonTier2Alt = []
-let patreonTier3Alt = []
-=======
 let patreonTier1 = ["tortulily", "invisibilia_draconis"]
 let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "graham anderson"]
 let patreonTier3 = ["froza"]
@@ -14,4 +5,3 @@ let patreonTier3 = ["froza"]
 let patreonTier1Alt = ["Dragomura"]
 let patreonTier2Alt = []
 let patreonTier3Alt = []
->>>>>>> Stashed changes
