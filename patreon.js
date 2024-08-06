d2cd37ee3fae8b3ee7f2115c9f9cc58a872cc927
SuperTurtleIdle <140896354+SuperTurtleIdle@users.noreply.github.com>
2024-08-06 21:03:00 +0200

    Update patreon.js

--- patreon.js
+++ patreon.js
@@ -1,5 +1,5 @@
 let patreonTier1 = ["tortulily", "huskymudkipper56", "nekosity", "kidsmeal", "pursuantaunt", "Jacozy", "Kasey"]
-let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "usajoyrider", "artgor", "mandlz", "maplecream.", "kgmg", "heinrich1960", "akerson", "johnnydela.", "occultjay", "rugiesm", "kevink6665", "Chrome", "Trusten"]
+let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "usajoyrider", "artgor", "mandlz", "maplecream.", "kgmg", "heinrich1960", "akerson", "johnnydela.", "occultjay", "rugiesm", "kevink6665", "Chrome", "Trusten", "snowdogdani"]
 let patreonTier3 = ["froza", "RC_Juicebox", "graham anderson", "E", "Steven Burton", "Sarah", "SaWright", "Okko Suga", "Nominae", "Ayse Gursoy", "Mike Muzurakis"]
 
 let patreonTier1Alt = ["Mudkip"]
