e54c4fe0e2ed0585ccb9512c8a3597aee2c15395
SuperTurtleIdle <140896354+SuperTurtleIdle@users.noreply.github.com>
2024-08-23 20:09:49 +0200

    Update patreon.js

--- patreon.js
+++ patreon.js
@@ -1,6 +1,6 @@
 let patreonTier1 = ["tortulily", "huskymudkipper56", "nekosity", "kidsmeal", "pursuantaunt", "Jacozy", "Kasey", "WubbleBubble"]
-let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "usajoyrider", "artgor", "mandlz", "maplecream.", "kgmg", "heinrich1960", "akerson", "johnnydela.", "occultjay", "rugiesm", "kevink6665", "Chrome", "Trusten", "snowdogdani", "kwazygloo", "simon_", "Marah"]
-let patreonTier3 = ["froza", "RC_Juicebox", "graham anderson", "E", "Steven Burton", "Sarah", "SaWright", "Okko Suga", "Nominae", "Ayse Gursoy", "Mike Muzurakis"]
+let patreonTier2 = ["jefeDeLaR", "penguin", "B1naryV1k1ng", "billyawn", "triplesixstorm", "kev4217", "usajoyrider", "artgor", "mandlz", "maplecream.", "kgmg", "heinrich1960", "akerson", "johnnydela.", "occultjay", "rugiesm", "kevink6665", "Chrome", "Trusten", "snowdogdani", "kwazygloo", "simon_", "Marah","Kai"]
+let patreonTier3 = ["froza", "graham anderson", "E", "Steven Burton", "Sarah", "SaWright", "Okko Suga", "Nominae", "Ayse Gursoy", "Mike Muzurakis", "Chris Taylor"]
 
 let patreonTier1Alt = ["Mudkip"]
 let patreonTier2Alt = []
